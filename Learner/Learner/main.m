c463d7c118b8e9f146364cddee2a7c87412c2280
胡洪轩 <1091018764@qq.com>
2016-04-16 14:20:34 +0800

    新建初始工程

---
+++ Learner/Learner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Learner
+//
+//  Created by 胡洪轩 on 16/4/16.
+//  Copyright © 2016年 LikeGod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
